541b28e6bf14eab4c4914cc5bc9e2fcc2dd1c1de
shootaroo <shotaro.tsubouchi@gmail.com>
2013-04-15 01:58:29 +0900

    support auto detect version of npm

--- get_npm.js
+++ get_npm.js
@@ -1,26 +1,28 @@
 var util = require('util'),
     fs = require('fs'),
     path = require('path'),
-    wget = require('./wget'),
-    npmVersions = require('./npm');
+    wget = require('./wget');
 
+var NPM_PKG_JSON_URL = 'https://raw.github.com/joyent/node/%s/deps/npm/package.json';
 var BASE_URL = 'http://nodejs.org/dist/npm/npm-%s.zip';
 
 var targetDir = process.argv[2];
 var nodeVersion = process.argv[3];
-var npmVersion = npmVersions[nodeVersion];
 
-if (!npmVersion) {
-  console.error('Node %s is not supported', nodeVersion);
-  process.exit(1);
-} else {
-  var uri = util.format(BASE_URL, npmVersion);
-  wget(uri, function (filename, data) {
-    fs.writeFile(path.join(targetDir, 'npm.zip'), data, function (err) {
-      if (err) {
-        return console.log(err.message);
-      }
-      console.log('Download npm %s is done', npmVersion);
+var pkgUri = util.format(NPM_PKG_JSON_URL, nodeVersion);
+wget(pkgUri, function (filename, pkg) {
+    if (filename === null) {
+        console.error('node %s is not include npm', nodeVersion);
+        process.exit(1);
+    }
+    var npmVersion = JSON.parse(pkg).version;
+    var uri = util.format(BASE_URL, npmVersion);
+    wget(uri, function (filename, data) {
+        fs.writeFile(path.join(targetDir, 'npm.zip'), data, function (err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            console.log('Download npm %s is done', npmVersion);
+        });
     });
-  });
-}
+});
